3a1a513bfa430bef2bdc8ce57a0b356ce9747a0b
Craig Irvine <cri646@mail.usask.ca>
2016-01-14 14:12:49 -0600

    Added RetrieveLog.php and RetrieveBroswerData.php scripts.  Both scripts are untested.

---
+++ Back_End/RetrieveLog.php
@@ -0,0 +1,70 @@
+<?PHP
+/***********************************************************************
+ * 	Script: RetrieveLog.php
+ * 	Description: Script for retrieving all log entries that match
+ *      passed filters.
+ *
+ *	Author: Craig Irvine (cri646@mail.usask.ca)
+ *	Date: 14 January 2016
+ *
+ *	Inputs: SID: The session ID of the client
+ *			LogLevel: The log level to retrieve logs for.
+ *
+ *	Usage: 	RetrieveLog.php?SID=<session id>&LogLevel=<log level>
+ ***********************************************************************/
+  
+include "IMSBase.php";
+include "IMSLog.php";
+include "IMSSql.php";
+
+
+$sessionID = "";
+$logLevel = "";
+
+$statusMessage = "";
+$statusCode = "";
+
+
+try
+{
+	if ($_SERVER["REQUEST_METHOD"] == "POST") 
+	{
+		$sessionID = $_POST["SID"];
+		$logLevel = $_POST["LogLevel"];  
+	}
+	
+	$IMSBase = new IMSBase();
+	$log = new IMSLog();
+
+	$IMSBase->verifyData($logLevel,"/^.+$/");
+	$IMSBase->verifyData($sessionID,"/^.+$/");
+	
+	$logArray = $log->read_log($logLevel);
+	
+        $statusCode = '0';
+	$statusMessage = 'RetrieveLog, successfully retrived log data.';
+	$log->add_log($sessionID,'Debug',$statusMessage);
+        
+}
+catch(PDOException $e)
+{
+	$statusCode = '1';
+	$statusMessage = 'RetrieveLog SQLError: '.$e->getMessage();
+	$log->add_log($sessionID,'Error',$statusMessage);
+	
+}
+catch(Exception $e)
+{
+	$statusCode = '1';
+	$statusMessage = 'RetrieveLog Error: '. $e->getMessage();
+	$log->add_log($sessionID,'Error',$statusMessage);
+
+}	
+//finally()  PHP 5.5+, currently using 5.3.
+//{
+	$statusArray[0] = $statusCode;
+	$statusArray[1] = $statusMessage;
+	//$dataArray will be null unless it was filled by $stmt->fetch()
+	$IMSBase->GenerateXMLResponse($sessionID,$statusArray,NULL,NULL,NULL,$logArray);
+//}	
+?>

86d14cf87ae534e8d01714de3936a11329c7d058
Craig Irvine <cri646@mail.usask.ca>
2016-03-05 17:27:18 -0600

    Email Sending
    - Added ability to send email using phpmailer.
    - Email settings in IMS_Settings.ini need to be set before an email can
    be sent.  Use your Usask credentials.
    - See Test_SendEmail.php in Back_End/testscripts for an example.
    - Added ignoring all *.log files in project directory to .gitignore.

--- Back_End/IMSBase.php
+++ Back_End/IMSBase.php
@@ -8,9 +8,15 @@
  *	Date: 08 January 2016
  *
  ***********************************************************************/
-
+require_once "vendor/autoload.php";
+ 
+ 
+ 
 class IMSBase
 {
+	private $php_options_file_loc = "IMS_Settings.ini";
+
+
 
 	public function verifyData($data,$RegEx,$optMessage = "")
 	{
@@ -101,7 +107,93 @@ class IMSBase
 
 		echo $xml->outputMemory(true);
 		
-}
+	}
+	
+	
+	public function sendEmail($to_array,$subject,$message)
+	{
+		
+	
+		$host = "";
+		$username = "";
+		$password = "";
+		$fromemail = "";
+		$fromname = "";
+		
+		if(file_exists($this->php_options_file_loc))
+		{
+			$options_file = parse_ini_file($this->php_options_file_loc,TRUE);	
+			
+			$host = $options_file["EMAIL_SETTINGS"]["EMAIL_HOST"];
+			$username = $options_file["EMAIL_SETTINGS"]["EMAIL_USER"];
+			$password = $options_file["EMAIL_SETTINGS"]["EMAIL_PASS"];
+			$fromemail = $options_file["EMAIL_SETTINGS"]["EMAIL_FROMEMAIL"];
+			$fromname = $options_file["EMAIL_SETTINGS"]["EMAIL_FROMNAME"];
+		}
+		else
+		{
+			throw new Exception("IMSBase->sendEmail: Could not find IMS_Settings.ini");
+		}
+		
+		
+		if($host == "")
+		{
+			throw new Exception("IMSBase->sendEmail: SMTP Host Name Missing");
+		}
+		if($username == "")
+		{
+			throw new Exception("IMSBase->sendEmail: SMTP User Name Missing");
+		}
+		if($password == "")
+		{
+			throw new Exception("IMSBase->sendEmail: SMTP Password Missing");
+		}
+		if($fromemail == "")
+		{
+			throw new Exception("IMSBase->sendEmail: SMTP From Email Missing");
+		}
+		if($fromname == "")
+		{
+			throw new Exception("IMSBase->sendEmail: SMTP From Name Missing");
+		}	
+	
+	
+		$mail = new PHPMailer(true); //Throw exceptions on error
+		
+		$mail->SMTPDebug = 0;
+		$mail->isSMTP();
+		$mail->Host = $host;		
+		$mail->SMTPAuth=true;
+		$mail->Username = $username;                 
+		$mail->Password = $password;
+		$mail->SMTPSecure = "tls";
+		$mail->Port = 587;
+		
+		
+		
+		$mail->From = $fromemail;
+		$mail->FromName = $fromname;
+		
+		
+		foreach($to_array as $to)
+		{
+			$mail->addAddress($to);
+		}
+		
+		
+		$mail->isHTML(true);
+		
+		$mail->Subject = $subject;
+		$mail->Body = $message;
+		$mail->AltBody = "Plain Text Email";	
+	
+		$mail->send();
+		
+		echo "mail sent";
+		
+	}
+	
+	
 
 }
 ?>
---
+++ Back_End/testscripts/Test_MailSend.php
@@ -0,0 +1,44 @@
+<?PHP
+/***********************************************************************
+ * 	Script: Test_MailSend.php
+ * 	Description: Script for testing the SendEmail function in IMSBase.
+ *
+ *	Author: Craig Irvine (cri646@mail.usask.ca)
+ *	Date: 24 Feb 2016
+ *
+ ***********************************************************************/
+ 
+include "IMSTest.php";
+include "../IMSBase.php";
+
+
+
+try
+{
+	$base = new IMSBase();
+	
+	
+	$subject = "Purchase Requirements";
+	
+	$message = 	"<p>This is a Purchase Requirements email</p>" .
+				"<table><tr><th>Date</th><th>Part</th><th>MFG PN</th><th>Qty</th></tr>" .
+			    "<tr><td>Date</td><td>Part</td><td>MFG PN</td><td>Qty</td></tr>" .
+			    "<tr><td>Date</td><td>Part</td><td>MFG PN</td><td>Qty</td></tr>" .			   
+			    "</table>";
+
+	$base->sendEmail(array("uktena.ren@gmail.com"),$subject,$message);
+}
+catch (phpmailerException $e)
+{
+	echo "phpmailerException: ".$e->errorMessage();
+
+}
+catch (Exception $e)
+{
+	echo "Exception: ".$e->getMessage();
+}
+
+
+ 
+?>
+
